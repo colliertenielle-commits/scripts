29bcd8ae78623c7d4b99c46f2d77b2ec7668010f
colliertenielle-commits <colliertenielle@gmail.com>
2026-02-04 09:10:55 -0600

    Update murder-mystery-2.lua
    Mm2

--- murder-mystery-2.lua
+++ murder-mystery-2.lua
@@ -1,1 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ethanoj1/EclipseMM2/master/Script", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Free-Keyless-Script/MurderMystery2/refs/heads/main/Main.lua"))()
